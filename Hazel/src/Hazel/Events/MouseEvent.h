a93b099a8da70726f9b7ffaad0c75ad5bef1ff56
victor <vigonetto@gmail.com>
2020-05-23 21:50:47 -0300

    Event System implementation.

---
+++ Hazel/src/Hazel/Events/MouseEvent.h
@@ -0,0 +1,6 @@
+#ifndef _MOUSE_EVENT_H
+#define _MOUSE_EVENT_H
+
+// To be done
+
+#endif // _MOUSE_EVENT_H
--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -2,7 +2,7 @@
 #define _LOG_H
 
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace Hazel {
 
