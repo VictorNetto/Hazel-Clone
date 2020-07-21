288f64cff9e197561355f4fca0c71fd782196743
victor <vigonetto@gmail.com>
2020-07-21 17:11:31 -0300

    Shader Asset Files

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -138,42 +138,9 @@ void InitShaders()
         }
     )";
 
-    std::string textureShaderVertexSrc = R"(
-        #version 450 core
-
-        layout (location = 0) in vec3 a_Position;
-        layout (location = 1) in vec2 a_TexCoord;
-
-        uniform mat4 u_ViewProjection;
-        uniform mat4 u_Transform;
-
-        out vec2 v_TexCoord;
-
-        void main()
-        {
-            v_TexCoord = a_TexCoord;
-            gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-        }
-    )";
-
-    std::string textureShaderFragmentSrc = R"(
-        #version 450 core
-
-        layout (location = 0) out vec4 color;
-
-        in vec2 v_TexCoord;
-
-        uniform sampler2D u_Texture;
-
-        void main()
-        {
-            color = texture(u_Texture, v_TexCoord);
-        }
-    )";
-
     shader.reset(Hazel::Shader::Create(vertexSrc, fragmentSrc));
     flatColorShader.reset(Hazel::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
-    textureShader.reset(Hazel::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+    textureShader.reset(Hazel::Shader::Create("Sandbox/assets/shaders/Texture.glsl"));
 
     std::dynamic_pointer_cast<Hazel::OpenGLShader>(textureShader)->Bind();
     std::dynamic_pointer_cast<Hazel::OpenGLShader>(textureShader)->UploadUniformInt("u_Texture", 0);
