38aeeab612de5269f49fcbe1a1d86a42b580b2af
victor <vigonetto@gmail.com>
2021-01-24 17:07:20 -0300

    Intro to Profiling + Visual Profiling 2.0

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -9,15 +9,15 @@ int main()
 {
     Hazel::Log::Init();
 
-    HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
+    HZ_PROFILE_BEGIN_SESSION("Startup", "Profiling/HazelProfile-Startup.json");
     auto app = Hazel::CreateApplication();
     HZ_PROFILE_END_SESSION();
 
-    HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
+    HZ_PROFILE_BEGIN_SESSION("Runtime", "Profiling/HazelProfile-Runtime.json");
     app->Run();
     HZ_PROFILE_END_SESSION();
 
-    HZ_PROFILE_BEGIN_SESSION("Shutdown", "HazelProfile-Shutdown.json");
+    HZ_PROFILE_BEGIN_SESSION("Shutdown", "Profiling/HazelProfile-Shutdown.json");
     delete app;
     HZ_PROFILE_END_SESSION();
 }
