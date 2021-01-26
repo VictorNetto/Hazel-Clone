1a640d51a6fba6c48e4f1bf0445ea6b8451b0a03
victor <vigonetto@gmail.com>
2021-01-25 22:43:32 -0300

    Improving our 2D Rendering API

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -43,8 +43,8 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
         Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
         Hazel::Renderer2D::DrawQuad({ -1.0f, 0.1f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-        Hazel::Renderer2D::DrawQuad({ 0.5f, 0.5f }, { 1.2f, 0.6f }, { 0.2f, 0.8f, 0.3f, 1.0f });
-        Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture);
+        Hazel::Renderer2D::DrawRotatedQuad({ 0.5f, 0.5f }, glm::radians(30.0f), { 1.2f, 0.6f }, { 0.2f, 0.8f, 0.3f, 1.0f });
+        Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture, 10.0f, { 0.7f, 0.5f, 0.7f, 1.0f });
 
         Hazel::Renderer2D::EndScene();
     }
