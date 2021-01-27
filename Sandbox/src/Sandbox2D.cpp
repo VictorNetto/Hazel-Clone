e95e2d1841054d29b5243baedf203ecd48ea71d5
victor <vigonetto@gmail.com>
2021-01-27 18:15:48 -0300

    BATCH RENDERING

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -5,9 +5,28 @@
 
 #include "imgui/imgui.h"
 
+#include <time.h>
+#include <stdlib.h>
+
+static float RandomFloat(float a, float b)
+{
+    // n is a random float number between 0.0f and 1.0f
+    float n = (float)rand() / (float)RAND_MAX;
+
+    return a + (b - a) * n;
+}
+
 Sandbox2D::Sandbox2D()
     : Layer("Sandbox2D"), m_CameraController(1200.0f / 900.0f, true)
 {
+    srand(time(NULL));
+
+    for (int i = 0; i < m_nQuads; i++)
+    {
+        m_QuadsProps[i].position = { RandomFloat(-3.0f, 3.0f), RandomFloat(-3.0f, 3.0f), RandomFloat(-0.1, 0.1) };
+        m_QuadsProps[i].size = { RandomFloat(-1.7f, 1.7f), RandomFloat(-1.7f, 1.7f) };
+        m_QuadsProps[i].color = { RandomFloat(0.0f, 1.0f), RandomFloat(0.0f, 1.0f), RandomFloat(0.0f, 1.0f), 1.0f };
+    }
 }
 
 void Sandbox2D::OnAttach()
@@ -33,7 +52,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
         HZ_PROFILE_SCOPE("Renderer Prep");
 
         // Render
-        Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+        Hazel::RenderCommand::SetClearColor({ 0.7f, 0.7f, 0.9f, 1.0f });
         Hazel::RenderCommand::Clear();
     }
 
@@ -42,9 +61,15 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 
         Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-        Hazel::Renderer2D::DrawQuad({ -1.0f, 0.1f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-        Hazel::Renderer2D::DrawRotatedQuad({ 0.5f, 0.5f }, glm::radians(30.0f), { 1.2f, 0.6f }, { 0.2f, 0.8f, 0.3f, 1.0f });
-        Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture, 10.0f, { 0.7f, 0.5f, 0.7f, 1.0f });
+        for (int i = 0; i < m_nQuads; i++)
+        {
+            glm::vec3 position = m_QuadsProps[i].position;
+            glm::vec2 size = m_QuadsProps[i].size;
+            glm::vec4 color = m_QuadsProps[i].color;
+            Hazel::Renderer2D::DrawQuad(position, size, color);
+        }
+        // Hazel::Renderer2D::DrawRotatedQuad({ 0.5f, 0.5f }, glm::radians(30.0f), { 1.2f, 0.6f }, { 0.2f, 0.8f, 0.3f, 1.0f });
+        // Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckboardTexture, 10.0f, { 0.7f, 0.5f, 0.7f, 1.0f });
 
         Hazel::Renderer2D::EndScene();
     }
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -1,5 +1,7 @@
 #include <Hazel.h>
 
+#include <array>
+
 class Sandbox2D : public Hazel::Layer
 {
 public:
@@ -15,4 +17,13 @@ public:
 private:
     Hazel::OrthographicCameraController m_CameraController;
     Hazel::Ref<Hazel::Texture2D> m_CheckboardTexture;
+
+    struct QuadProp
+    {
+        glm::vec3 position;
+        glm::vec2 size;
+        glm::vec4 color;
+    };
+    const int m_nQuads = 200;
+    std::array<QuadProp, 200> m_QuadsProps;
 };
